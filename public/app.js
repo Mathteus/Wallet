2cf49ded9d5d52542d7b1c87c5fdab626006048e
Matheus Henrique <85768227+Mathteus@users.noreply.github.com>
2024-04-29 14:03:41 -0300

    Delete public directory

--- public/app.js
+++
@@ -1,6 +0,0 @@
-const utils = require('../server/utils.js')
-
-utils.get("#logggin-btn").onclick = async () => {
-  const response = await utils.verifyLogin(utils.get("#username").value, utils.get("#password").value);
-  console.log(response);
-};
